0abd64f38c5003c776a4e20e904e358a4d37ae00
Raul Gracia <raul.gracia@gamesys.co.uk>
2012-10-30 15:04:37 +0000

    About Triangle 2

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,7 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  check_errors(a,b,c)
   return :equilateral if equilateral?(a,b,c)
   return :isosceles if isosceles?(a,b,c)
   return :scalene if scalene?(a,b,c)
@@ -33,6 +34,11 @@ def scalene?(a, b, c)
   a != b && b != c && c != a
 end
 
+def check_errors(a,b,c)
+  raise TriangleError, "side with lenght 0" if a == 0 || b == 0 || c ==0
+  raise TriangleError, "negative side" if a < 1 || b < 1 || c < 1
+  raise TriangleError, "No two sides can add to be less than or equal to the other side" if (a+b <= c) || (a+c <= b) || (b+c <= a)
+end
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
