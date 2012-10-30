ca5275a006ac31c5a4e97ccd5a036f772ec579fc
Raul Gracia <raul.gracia@gamesys.co.uk>
2012-10-30 14:19:11 +0000

    About Triangle

--- triangle.rb
+++ triangle.rb
@@ -14,8 +14,25 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  return :equilateral if equilateral?(a,b,c)
+  return :isosceles if isosceles?(a,b,c)
+  return :scalene if scalene?(a,b,c)
 end
+
+def equilateral?(a, b, c)
+  a == b && b == c
+end
+
+def isosceles?(a, b, c)
+  (a == b && b != c) || 
+  (b == c && c != a) || 
+  (c == a && a != b)
+end
+
+def scalene?(a, b, c)
+  a != b && b != c && c != a
+end
+
 
 # Error class used in part 2.  No need to change this code.
 class TriangleError < StandardError
